394dd483630fac69618b54e911a5cdc0b59c80a0
IotaSphere <40618095+Geocube101@users.noreply.github.com>
2025-09-09 15:37:43 -0400

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,8 @@
+{
+  let background = document.getElementsByClassName('background-image')[0].querySelector('img');
+  background.style.top = `-${window.scrollY / 8}px`;
+
+  window.addEventListener('scroll', (e) => {
+    background.style.top = `-${window.scrollY / 8}px`;
+  });
+}
